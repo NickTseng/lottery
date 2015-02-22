a51ca6cab17e29a19e212f132a468d6f9617e543
nick.tseng <z77819@gmail.com>
2015-02-22 21:34:34 +0800

    heroku

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ app.controller('ctl', function($scope, $filter, $document) {
 
     $scope.numberArray = [];
     $scope.set = [];
-    $scope.setGroup = [
-        []
-    ];
+    $scope.setGroup = [];
     // $scope.number = '';
 
 
@@ -29,40 +27,70 @@ app.controller('ctl', function($scope, $filter, $document) {
 
     $scope.addToArray = function(value) {
 
-        // debugger;
+        if ($scope.setGroup.length == 0) {
+            $scope.setGroup.push([]);
+        }
         $scope.setGroup[$scope.setGroup.length - 1].push(value);
 
 
         var exist = $filter('filter')($scope.set, {
             key: value
         });
-        // debugger;
 
 
         exist[0].count += 1;
-
     };
+
+
+    $scope.deleteSet = function(index) {
+        var set = $scope.setGroup[index];
+
+        // remove
+        $scope.setGroup.splice(index, 1);
+
+        angular.forEach(set, function(value, key) {
+            // this.push(key + ': ' + value);
+
+            var exist = $filter('filter')($scope.set, {
+                key: value
+            });
+            exist[0].count -= 1;
+
+        });
+    }
+
+    // $scope.minusToArray = function(value) {
+
+
+    // }
 
 
     $scope.$watch("number", function(newValue, oldValue) {
 
         // debugger;
         var str = newValue.toString();
+        // debugger;
+        if ((str.length > 2) || (isNaN(newValue) || !angular.isNumber(+newValue))) {
+
+            $scope.number = oldValue;
+            return;
+        }
 
         if (newValue == 0 || newValue > 49) {
-
+            $scope.number = '';
             return
         }
 
-        if ((str.length == 2)) {
+        if (str.length == 2) {
 
             $scope.addToArray(newValue);
             $scope.number = '';
-
-        } else {
-
-            $scope.number = newValue;
+            return;
         }
+
+
+        $scope.number = newValue;
+
 
     });
 
@@ -72,7 +100,7 @@ app.controller('ctl', function($scope, $filter, $document) {
 
         $scope.set = [];
         $scope.setGroup = [
-            []
+
         ];
     }
 
@@ -83,6 +111,16 @@ app.controller('ctl', function($scope, $filter, $document) {
     }
 
 
+    $scope.isTop = function(value) {
+        var top = $filter('orderBy')($scope.set, 'count', true);
+        // debugger;
 
+        for (var i = 0; i < 5; i++) {
+            if (top[i].key == value) {
+                return true;
+            }
+        };
 
+        return false;
+    }
 });
